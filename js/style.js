3c656b20db8c4c778ae92d1834ce84abb073355f
natsumiegashira <79183113+natsumiegashira@users.noreply.github.com>
2021-03-05 17:00:51 +0900

    Update style.js

--- js/style.js
+++ js/style.js
@@ -52,14 +52,18 @@ $(function(){
 	});
 });
 
-
-
-$(".qa-list dd").hide();
-$(".qa-list dl").on("click", function(e){
-    $('dd',this).slideToggle('fast');
-    if($(this).hasClass('open')){
-        $(this).removeClass('open');
-    }else{
-        $(this).addClass('open');
-    }
+$(function(){
+	$(".qa-list dd").hide();
+	$(".qa-list dl").on("click", function(e){
+	    $('dd',this).slideToggle('fast');
+	    if($(this).hasClass('open')){
+		$(this).removeClass('open');
+	    }else{
+		$(this).addClass('open');
+	    }
+	});	
 });
+
+
+
+
